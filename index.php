7c8dab70b1580632bdbc7c854228a13038adcc27
JD Flynn <jflynn8@gmail.com>
2018-08-10 08:06:22 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'inc/head.html' ?>
+
+
+<body>
+<div id="page">
+	
+<?php include 'inc/header.html' ?>
+
+
+
+<main id="main" class="site-main">
+
+<?php include 'inc/carousel.html' ?>
+
+<?php include 'inc/about.html' ?>
+
+<?php include 'inc/testimonials.html' ?>
+
+<?php include 'inc/menu.html' ?>
+
+<?php include 'inc/crew.html' ?>
+
+<!-- Hide Vendors
+
+
+<?php include 'inc/vendors.html' ?>
+
+-->
+
+<?php include 'inc/contact.html' ?>
+		
+</main>
+
+<?php include 'inc/footer.html' ?>
+
+</div>
+
+<div class="scroll-to-top" data-spy="affix" data-offset-top="200"><a href="#page" class="smooth-scroll"><i class="fa fa-arrow-up"></i></a></div>
+  
+<?php include 'inc/js.html' ?>
+
+</html>
